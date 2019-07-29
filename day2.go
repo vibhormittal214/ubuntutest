9f71d7b715fdc80ca9e6f9a41e8c7413c0727b59
vibhormittal214 <vibhormittal214@gmail.com>
2019-07-29 23:32:13 +0530

    11th

--- day2.go
+++ day2.go
@@ -4,8 +4,11 @@ func main(){
 	var a int=5;
 	fmt.Printf("%d",a);
 	var b int =6;
+	var strr string="geeks";
+	fmt.Printf("%s",strr);
 	fmt.Printf("%d",b);
-	var c=5;
-	var mm=10;
-	var d=8;
+//	var c=5;
+//	var mm=10;
+//	var d=8;
+
 }
