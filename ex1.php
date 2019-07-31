157a7fcef12ab66b671cd682ba23292bc36808e9
vibhormittal214 <vibhormittal214@gmail.com>
2019-07-31 09:14:04 +0530

    new 1

--- ex1.php
+++ ex1.php
@@ -7,6 +7,7 @@
    	   }
    	   function set($a){
            $this->b=$a;
+           echo " testing";
    	   }
    }
    class B{
@@ -17,5 +18,8 @@
    }
    function test(){
    	   echo "this is a test funcion";
+   }
+   function test1(){
+   	echo "this is another test function";
    }
    ?>
