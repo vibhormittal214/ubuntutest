7aca54706fea5fc1c8a2e5cf7d214dd2c386e6cc
vibhormittal214 <vibhormittal214@gmail.com>
2019-07-31 11:33:19 +0530

    frf

--- ex2.php
+++ ex2.php
@@ -2,11 +2,12 @@
      $a=5;
      function test1($a){
      	echo $a;
-          echo "no";
+          echo "no2";
      }
      function test2(){
      	echo "hello";
      	$c=3;
      	test1($b);
+          echo "yes1";
      }
      ?>
