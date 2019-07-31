255b6d65b6abe4868914f820503ed80d7a263af2
vibhormittal214 <vibhormittal214@gmail.com>
2019-07-31 11:36:47 +0530

    1234678

--- ex2.php
+++ ex2.php
@@ -1,14 +1,14 @@
 <?php
      $a=5;
      $b=6;
-     $c=7;
+     $c=8;
      function test1($a){
      	echo $a;
           echo "no2";
           echo "no1";
      }
      function test2(){
-     	echo "hello";
+     	echo "yes";
      	$c=3;
      	test1($b);
           echo "yes1";
